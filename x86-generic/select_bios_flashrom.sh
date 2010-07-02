a488ffc5e733ed4d254a41c7c7231aa05aea037a
Hung-Te Lin <hungte@chromium.org>
2010-07-02 21:24:08 +0800

    update BIOS/EC selection script to fix extra pci_write commands
    Review URL: http://codereview.chromium.org/2854036

--- x86-generic/select_bios_flashrom.sh
+++ x86-generic/select_bios_flashrom.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -10,9 +10,9 @@ if [ -x "$custom_script" ]; then
   exec "$custom_script"
 elif [ -r "$custom_script" ]; then
   exec /bin/sh "$custom_script"
+  # never returns here
 fi
 
 mmio_write32 0xfed1f410 `mmio_read32 0xfed1f410 |head -c 6`0460
-pci_write32 0 31 0 0xd0 0x0111223e
 mmio_read32 0xfed1f410
 pci_read32 0 31 0 0xd0
