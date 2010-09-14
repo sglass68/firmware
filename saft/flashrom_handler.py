fe816ecbe4c3467dbbbbb515703de188e1c419e1
Duncan Laurie <dlaurie@chromium.org>
2010-09-14 15:13:37 -0700

    Fix typo: formware -> firmware
    BUG=
    TEST=docstring change only
    
    Change-Id: I3525921f6fe299d5a4355461204c79454c86106e
    
    Review URL: http://codereview.chromium.org/3386002

--- saft/flashrom_handler.py
+++ saft/flashrom_handler.py
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-'''A module to support automated testing of ChromeOS formware.
+'''A module to support automated testing of ChromeOS firmware.
 
 Utilizes services provided by saft_flashrom_util.py read/write the
 flashrom chip and to parse the flash rom image.
