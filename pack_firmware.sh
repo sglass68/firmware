46d127328609f78a21c990d3fb160c2d94d89e3c
Hung-Te Lin <hungte@chromium.org>
2011-04-13 08:24:16 +0800

    chromeos-firmware: refine firmware updater packer and support multiple tool folders
    To support fetching binary tool programs from different folders,
    we need to extend the --tool_base for a colon-delimited string.
    
    --flashrom is also deprecated and replaced by --tool "flashrom mosys...", for
    more flexibility because we may want to prevent embedding any tool programs.
    
    There're also code refine to make the updater script more compliant to the shell
    script coding style guide, and more compatible.
    
    BUG=chromium-os:14014
    TEST=emerge-x86-XXX chromeos-firmware; # seeing same packaged output
         ./pack_firmware --tool_base=.../usr/bin:.../usr/sbin: \
           --tools "flashrom mosys crossystem" .... # success
    
    Change-Id: Ib25fc6216332bcaf2bb709b4ce9f180986a88cb9
    
    R=rspangler@chromium.org
    
    Review URL: http://codereview.chromium.org/6821032

--- pack_firmware.sh
+++ pack_firmware.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 # This script packages firmware images into an executale "shell-ball".
 # It requires:
 #  - at least one BIOS image and/or EC image (*.bin / *.fd)
-#  - flashrom(8) as native binary tool
-#  - pack_dist/updater.sh script to execute flashrom(8)
+#  - pack_dist/updater.sh main script
 #  - pack_stub as the template/stub script for output
 #  - any other additional files used by updater.sh in pack_dist folder
 
@@ -24,113 +23,126 @@ DEFINE_string ec_version "IGNORE" "Version of input EC firmware image"
 DEFINE_string output "-" "Path of output filename; '-' for stdout" "o"
 DEFINE_string extra "" "Directory list (separated by :) of files to be merged"
 
-# TODO(hungte) add 'crossytem' into embedded tools
-
-# tools
-DEFINE_string flashrom "" \
-  "Path of flashrom(8), using tool_base/flashrom if not assigned"
-DEFINE_string tools "mosys" \
+# embedded tools
+# TODO(hungte) add crossystem after we've updated the ebuild files
+DEFINE_string tools "flashrom mosys" \
   "List of tool programs to be bundled into updater"
-DEFINE_string tool_base "" "Default source location for tools programs"
+DEFINE_string tool_base "" \
+  "Default source locations for tools programs (delimited by colon)"
 
 # Parse command line
 FLAGS "$@" || exit 1
 eval set -- "${FLAGS_ARGV}"
 
-# backward-compatible
-if [ -n "${FLAGS_board}" -a -z "${FLAGS_tool_base}" ]; then
-  FLAGS_tool_base=/build/${FLAGS_board}/usr/sbin
-fi
-
-# provide default location of flashrom location
-if [ "${FLAGS_flashrom}" == "" ]; then
-  FLAGS_flashrom="${FLAGS_tool_base}/flashrom"
-fi
-
 # we need following tools to be inside a package:
-#  - flashrom(8): native binary tool
 #  - $FLAGS_tools: native binary tools
 #  - pack_dist/updater.sh
 #  - all files in pack_dist (usually used by updater.sh like selectors)
 
-pack_dist="$script_base/pack_dist"
-flashrom_bin="${FLAGS_flashrom}"
-main_script="$pack_dist/updater.sh"
 stub_file="$script_base/pack_stub"
+pack_dist="$script_base/pack_dist"
+main_script="$pack_dist/updater.sh"
+tmpbase=""
 
-function do_cleanup {
+# helper utilities
+
+do_cleanup() {
   if [ -d "$tmpbase" ]; then
     rm -rf "$tmpbase"
+    tmpbase=""
   fi
 }
 
-function err_die {
-  if [ "$1" != "" ]; then
-    echo "$1" >&2
+err_die() {
+  if [ -n "$1" ]; then
+    echo "ERROR: $1" >&2
   fi
-  do_cleanup
   exit 1
 }
 
+has_command() {
+  type "$1" >/dev/null 2>&1
+}
+
+find_tool() {
+  local toolbase_list="$(echo "$FLAGS_tool_base" | tr ':' '\n')"
+  local toolpath
+  toolpath="$(echo "$toolbase_list" |
+              while read toolbase; do
+                if [ -f "$toolbase/$1" ]; then
+                  echo "$toolbase/$1"
+                  exit $FLAGS_TRUE
+                fi
+              done)"
+  [ -n "$toolpath" ] && echo "$toolpath"
+}
+
+trap do_cleanup EXIT
+
 # check tool: we need uuencode to create the shell ball.
-type -P uuencode >/dev/null || err_die "ERROR: You need uuencode(sharutils)"
+has_command uuencode || err_die "You need uuencode(sharutils)"
 
 # check required basic files
-for X in "$flashrom_bin" "$main_script" "$stub_file"; do
+for X in "$main_script" "$stub_file"; do
   if [ ! -r "$X" ]; then
-    err_die "ERROR: Cannot find required file: $X"
+    err_die "Cannot find required file: $X"
   fi
 done
 
 # check tool programs
+flashrom_bin=""
 for X in $FLAGS_tools; do
-  if [ ! -r "${FLAGS_tool_base}/$X" ]; then
-    err_die "ERROR: Cannot find tool program: ${FLAGS_tool_base}/$X"
+  if ! find_tool "$X" >/dev/null; then
+    err_die "Cannot find tool program to bundle: $X"
+  fi
+  if [ "$X" = "flashrom" ]; then
+    flashrom_bin="$(find_tool $X)"
   fi
 done
 
 # check input: fail if no any images were assigned
 bios_bin="${FLAGS_bios_image}"
 ec_bin="${FLAGS_ec_image}"
-if [ "$bios_bin" == "" -a "$ec_bin" == "" ]; then
-  err_die "ERROR: must assign at least one of BIOS or EC image."
+if [ "$bios_bin" = "" -a "$ec_bin" = "" ]; then
+  err_die "must assign at least one of BIOS or EC image."
 fi
 
 # create temporary folder to store files
-tmpfolder="`mktemp -d`" || err_die "Cannot create temporary folder."
-
-# XXX only define 'tmpbase' after we've successfully created it because err_die
-# will destroy $tmpbase.
-tmpbase="$tmpfolder"
+tmpbase="$(mktemp -d)" || err_die "Cannot create temporary folder."
 version_file="$tmpbase/VERSION"
-flashrom_ver="$(strings "$flashrom_bin" |
-                grep '^[0-9\.]\+ \+: \+[a-z0-9]\+ \+: \+.\+UTC')"
-echo "Package create date: `date +'%c'`
-
+echo "Package create date: $(date +'%c')" >>"$version_file"
+if [ -n "$flashrom_bin" ]; then
+  flashrom_ver="$(
+    strings "$flashrom_bin" |
+    grep '^[0-9\.]\+ \+: \+[a-z0-9]\+ \+: \+.\+UTC')"
+  echo "
 flashrom(8): $(md5sum -b "$flashrom_bin")
              $(file -b "$flashrom_bin")
-             $flashrom_ver" >> "$version_file"
+             $flashrom_ver" >>"$version_file"
+fi
+echo "" >>"$version_file"
 
 # copy firmware image files
 if [ "$bios_bin" != "" ]; then
-  cp "$bios_bin" "$tmpbase/bios.bin" || err_die "cannot get BIOS image"
+  cp -pf "$bios_bin" "$tmpbase/bios.bin" || err_die "cannot get BIOS image"
   echo "BIOS image:  $(md5sum -b "$bios_bin")" >> "$version_file"
 fi
 if [ "$ec_bin" != "" ]; then
-  cp "$ec_bin" "$tmpbase/ec.bin" || err_die "cannot get EC image"
+  cp -pf "$ec_bin" "$tmpbase/ec.bin" || err_die "cannot get EC image"
   echo "EC image:    $(md5sum -b "$ec_bin")" >> "$version_file"
 fi
 
-# copy other resources files from pack_dist
-# XXX do not put any files with dot in prefix ( eg: .blah )
-cp -p "$flashrom_bin" "$tmpbase"/flashrom || err_die "cannot copy tool flashrom"
-cp -p "$SHFLAGS_FILE" "$tmpbase"/. || err_die "cannot copy shflags"
+# copy tool programs and main resources from pack_dist.
+# WARNING: do not put any files with dot in prefix ( eg: .blah )
+cp -pf "$SHFLAGS_FILE" "$tmpbase"/. || err_die "cannot copy shflags"
 for X in $FLAGS_tools; do
-  src="${FLAGS_tool_base}/$X"
-  cp -p "$src" "$tmpbase"/. || err_die "cannot copy tools: $src"
+  cp -pf "$(find_tool "$X")" "$tmpbase"/. || err_die "cannot copy $X"
+  chmod a+rx "$tmpbase/$X"
 done
-cp -rp "$pack_dist"/* "$tmpbase" || err_die "cannot copy pack_dist folder"
-chmod a+rx "$tmpbase"/flashrom "$tmpbase"/updater.sh
+cp -rfp "$pack_dist"/* "$tmpbase" || err_die "cannot copy pack_dist folder"
+
+# adjust file permission
+chmod a+rx "$tmpbase"/*.sh
 
 # copy extra files. if $FLAGS_extra is a folder, copy all content inside.
 extra_list="$(echo "${FLAGS_extra}" | tr ':' '\n')"
@@ -141,10 +153,11 @@ for extra in $extra_list; do
     echo "Extra files from folder: $extra" >> "$version_file"
   elif [ "$extra" != "" ]; then
     cp -r "$extra" "$tmpbase" || \
-      err_die "cannot copy extra files $extra"
+      err_die "Cannot copy extra files $extra"
     echo "Extra file: $extra" >> "$version_file"
   fi
 done
+chmod -R a+r "$tmpbase"/*
 
 # create MD5 checksum logs
 echo "
@@ -154,24 +167,22 @@ Package Content:" >> "$version_file"
 
 # package temporary folder into ouput file
 # TODO(hungte) use 'shar' instead?
-output="${FLAGS_output}"
-if [ "$output" == "-" ]; then
-  (cat "$stub_file"  | sed -e "s/REPLACE_FWVERSION/${FLAGS_bios_version}/" \
-     -e "s/REPLACE_ECVERSION/${FLAGS_ec_version}/" &&
-   tar zcf - -C "$tmpbase" . | uuencode firmware_package.tgz) ||
-  err_die "ERROR: Failed to archive firmware package"
+if [ -z "${FLAGS_output}" -o "${FLAGS_output}" = "-" ]; then
+  output_opt=""
 else
-  # we can provide more information when output is not stdout
-  (cat "$stub_file"  | sed -e "s/REPLACE_FWVERSION/${FLAGS_bios_version}/" \
+  output_opt="of=${FLAGS_output}"
+fi
+output="${FLAGS_output}"
+(cat "$stub_file" |
+ sed -e "s/REPLACE_FWVERSION/${FLAGS_bios_version}/" \
      -e "s/REPLACE_ECVERSION/${FLAGS_ec_version}/" &&
-   tar zcf - -C "$tmpbase" . | uuencode firmware_package.tgz) > "$output" ||
-   err_die "ERROR: Failed to archive firmware package"
+ tar zcf - -C "$tmpbase" . | uuencode firmware_package.tgz) |
+ dd $output_opt 2>/dev/null || err_die "Failed to archive firmware package"
+
+# we can provide more information when output is not stdout
+if [ -n "$output_opt" ]; then
   chmod a+rx "$output"
   cat "$tmpbase/VERSION"
   echo ""
   echo "Packed output image is: $output"
 fi
-
-# clean resources
-do_cleanup
-exit 0
