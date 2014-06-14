f588f117594166461a668c3d20c29baf09edbe8a
Hung-Te Lin <hungte@chromium.org>
2014-06-15 04:39:34 +0800

    updater: Add unit tests for crosutil.
    Add unit tests for functions defined in crosutil.sh.
    
    BUG=chromium:193120
    TEST=(chroot) cd unittests; ./crosutil_unittest
    
    Change-Id: I2d822798542bb01a4f762fa7549be322be9fda0e
    Reviewed-on: https://chromium-review.googlesource.com/203906
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: Randall Spangler <rspangler@chromium.org>
    Commit-Queue: Hung-Te Lin <hungte@chromium.org>

--- pack_dist/common.sh
+++ pack_dist/common.sh
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-SCRIPT_BASE="$(dirname "$0")"
+: ${SCRIPT_BASE:="$(dirname "$0")"}
 if [ -s "$SCRIPT_BASE/shflags" ]; then
   . "$SCRIPT_BASE/shflags"
 else
