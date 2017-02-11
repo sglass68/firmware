a3442f87cd5834ec19d0b5c2032e196e92dfa463
Simon Glass <sjg@chromium.org>
2017-02-11 10:24:18 -0700

    wip
    Change-Id: I1b77500e6447bc745d2645926038f46cfeb7c486

--- pack_firmware.py
+++ pack_firmware.py
@@ -12,62 +12,108 @@ It requires:
 """
 
 import argparse
+import os
+import sys
+import uu
 
-# Parse the available arguments:
-# Invalid arguments or -h cause this function to print a message and exit.
-# Returns:
-#   argparse.Namespace object containing the attributes.
-def parse_args():
-  parser = argparse.ArgumentParser(
-      description='Produce a firmware update shell-ball')
-  parser.add_argument('-b', '--bios_image', type=str,
-                      help='Path of input AP (BIOS) firmware image')
-  parser.add_argument('-w', '--bios_rw_image', type=str,
-                      help='Path of input BIOS RW firmware image')
-  parser.add_argument('-e', '--ec_image', type=str,
-                      help='Path of input Embedded Controller firmware image')
-  parser.add_argument('-p', '--pd_image', type=str,
-                      help='Path of input Power Delivery firmware image')
-  parser.add_argument('--script', type=str, default='updater.sh',
-                      help='File name of main script file')
-  parser.add_argument('-o', '--output', type=str,
-                      help='Path of output filename')
-  parser.add_argument(
-      '--extra', type=str,
-      help='Directory list (separated by :) of files to be merged')
+from chromite.lib import cros_build_lib
+import chromite.lib.cros_logging as logging
 
-  parser.add_argument('--remove_inactive_updaters', action='store_true',
-                      help='Remove inactive updater scripts')
-  parser.add_argument('--create_bios_rw_image', action='store_true',
-                      help='Resign and generate a BIOS RW image')
-  merge_parser = parser.add_mutually_exclusive_group(required=False)
-  merge_parser.add_argument(
-      '--merge_bios_rw_image', default=True, action='store_true',
-      help='Merge the --bios_rw_image into --bios_image RW sections')
-  merge_parser.add_argument('--no-merge_bios_rw_image', action='store_false',
-                      dest='merge_bios_rw_image',
-                      help='Resign and generate a BIOS RW image')
+class PackFirmware:
+  def __init__(self, progname):
+    self.script_base = os.path.dirname(progname)
+    self.stub_file = os.path.join(self.script_base, 'pack_stub')
+    self.pack_dist = os.path.join(self.script_base, 'pack_dist')
+  
+  # Parse the available arguments:
+  # Invalid arguments or -h cause this function to print a message and exit.
+  # Returns:
+  #   argparse.Namespace object containing the attributes.
+  def ParseArgs(self, argv):
+    parser = argparse.ArgumentParser(
+        description='Produce a firmware update shell-ball')
+    parser.add_argument('-b', '--bios_image', type=str,
+                        help='Path of input AP (BIOS) firmware image')
+    parser.add_argument('-w', '--bios_rw_image', type=str,
+                        help='Path of input BIOS RW firmware image')
+    parser.add_argument('-e', '--ec_image', type=str,
+                        help='Path of input Embedded Controller firmware image')
+    parser.add_argument('-p', '--pd_image', type=str,
+                        help='Path of input Power Delivery firmware image')
+    parser.add_argument('--script', type=str, default='updater.sh',
+                        help='File name of main script file')
+    parser.add_argument('-o', '--output', type=str,
+                        help='Path of output filename')
+    parser.add_argument(
+        '--extra', type=str,
+        help='Directory list (separated by :) of files to be merged')
 
-  # stable settings
-  parser.add_argument('--stable_main_version', type=str,
-                      help='Version of stable main firmware')
-  parser.add_argument('--stable_ec_version', type=str,
-                      help='Version of stable EC firmware')
-  parser.add_argument('--stable_pd_version', type=str,
-                      help='Version of stable PD firmware')
+    parser.add_argument('--remove_inactive_updaters', action='store_true',
+                        help='Remove inactive updater scripts')
+    parser.add_argument('--create_bios_rw_image', action='store_true',
+                        help='Resign and generate a BIOS RW image')
+    merge_parser = parser.add_mutually_exclusive_group(required=False)
+    merge_parser.add_argument(
+        '--merge_bios_rw_image', default=True, action='store_true',
+        help='Merge the --bios_rw_image into --bios_image RW sections')
+    merge_parser.add_argument('--no-merge_bios_rw_image', action='store_false',
+                        dest='merge_bios_rw_image',
+                        help='Resign and generate a BIOS RW image')
 
-  # embedded tools
-  parser.add_argument('--tools', type=str,
-      default='flashrom mosys crossystem gbb_utility vpd dump_fmap',
-      help='List of tool programs to be bundled into updater')
+    # stable settings
+    parser.add_argument('--stable_main_version', type=str,
+                        help='Version of stable main firmware')
+    parser.add_argument('--stable_ec_version', type=str,
+                        help='Version of stable EC firmware')
+    parser.add_argument('--stable_pd_version', type=str,
+                        help='Version of stable PD firmware')
 
-  # TODO(sjg@chromium.org: Consider making this accumulate rather than using
-  # the ':' separator.
-  parser.add_argument(
-      '--tool_base', type=str,
-      help='Default source locations for tools programs (delimited by colon)')
-  return parser.parse_args()
+    # embedded tools
+    parser.add_argument('--tools', type=str,
+        default='flashrom mosys crossystem gbb_utility vpd dump_fmap',
+        help='List of tool programs to be bundled into updater')
+
+    # TODO(sjg@chromium.org: Consider making this accumulate rather than using
+    # the ':' separator.
+    parser.add_argument(
+        '--tool_base', type=str,
+        help='Default source locations for tools programs (delimited by colon)')
+    return parser.parse_args(argv)
+
+  def _HasCommand(self, cmd, package):
+    result = cros_build_lib.RunCommand('type %s' % cmd, shell=True, quiet=True,
+                                       error_code_ok=True)
+    if result.returncode:
+      logging.critical('You need %s (%s)' % (cmd, package))
+      return False
+    return True
+
+  def _FindTool(self, tool):
+    
+
+  def _FindTools(self, tools):
+    for tool in tools:
+
+  def Start(self, argv):
+    args = self.ParseArgs(argv)
+    main_script = os.path.join(self.pack_dist, args.script)
+
+    if not self._HasCommand('shar', 'sharutils'):
+      return False
+    for fname in [main_script, self.stub_file]:
+      if not os.path.exists(fname):
+        logging.critical("Cannot find required file '%s'" % fname)
+        return False
+
+    return True
 
 
-args = parse_args()
-print(args)
+# The style guide says that we cannot pass in sys.argv[0]. That mains testing
+# a pain, so this is a full argv.
+def main(argv):
+  pack = PackFirmware(argv[0])
+  return pack.Start(argv[1:])
+
+if __name__ == "__main__":
+  if not main(sys.argv):
+    sys.exit(1)
---
+++ pack_firmware_unittest.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+from contextlib import contextmanager
+from StringIO import StringIO
+import sys
+import unittest
+
+import chromite.lib.cros_logging as logging
+import pack_firmware
+from pack_firmware import PackFirmware
+
+# Disable all logging as it's confusing to get log output from tests.
+logging.getLogger().setLevel(logging.CRITICAL + 1)
+
+
+# Use this to suppress stdout/stderr output:
+# with capture_sys_output() as (stdout, stderr)
+#   ...do something...
+@contextmanager
+def capture_sys_output():
+    capture_out, capture_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = capture_out, capture_err
+        yield capture_out, capture_err
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+class TestUnit(unittest.TestCase):
+  def setUp(self):
+    self.pack = PackFirmware('.')
+
+  def testStartup(self):
+    """Starting up with a valid updater script should work."""
+    self.assertFalse(pack_firmware.main(['.']))
+    self.assertTrue(pack_firmware.main(['.', '--script=updater5.sh']))
+
+  def testBadStartup(self):
+    """Starting up in another directory (without required files) should fail."""
+    self.assertFalse(pack_firmware.main(['/']))
+
+  def testArgParse(self):
+    """Test some basic argument parsing as a sanity check."""
+    with self.assertRaises(SystemExit):
+      with capture_sys_output() as (stdout, stderr):
+        self.assertEqual(None, self.pack.ParseArgs(['--invalid']))
+
+    self.assertEqual(None, self.pack.ParseArgs([]).bios_image)
+    self.assertEqual('bios.bin',
+                     self.pack.ParseArgs(['-b', 'bios.bin']).bios_image)
+
+    self.assertEqual(False, self.pack.ParseArgs([]).remove_inactive_updaters)
+    self.assertEqual(True,
+                     self.pack.ParseArgs(['--remove_inactive_updaters'])
+                         .remove_inactive_updaters)
+
+    self.assertEqual(True, self.pack.ParseArgs([]).merge_bios_rw_image)
+    self.assertEqual(True, self.pack.ParseArgs(['--merge_bios_rw_image'])
+                         .merge_bios_rw_image)
+    self.assertEqual(False, self.pack.ParseArgs(['--no-merge_bios_rw_image'])
+                         .merge_bios_rw_image)
+
+  def testHasCommand(self):
+    self.assertTrue(self.pack.HasCommand('ls', 'sample-package'))
+    self.assertFalse(self.pack.HasCommand('does-not-exist', 'sample-package'))
+
+if __name__ == '__main__':
+    unittest.main()
