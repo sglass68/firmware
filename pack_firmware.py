89a38bb7b622c4dd8543cc312047d8049f8fb31f
Simon Glass <sjg@chromium.org>
2017-03-10 12:41:00 -0700

    pack_firmware.py: Correct handling of EC/PD RW merging
    There is a typo in the CBFS section name which causes this to produce an
    empty version. Correct this and expand the test to catch it.
    
    BUG=chromium:690573
    TEST=PYTHONPATH=~/cosarm python pack_firmware_unittest.py
    PYTHONPATH=~/cosarm python pack_firmware_functest.py
    Change-Id: I8a368873bba52c4ad79ef0954249d556b1a7c15a
    Signed-off-by: Simon Glass <sjg@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/452968
    Reviewed-by: Dan Erat <derat@chromium.org>

--- pack_firmware.py
+++ pack_firmware.py
@@ -533,7 +533,7 @@ class FirmwarePacker(object):
         self._MergeRwEcFirmware(self._BaseDirPath(IMAGE_EC),
                                 self._BaseDirPath(IMAGE_MAIN), 'ecrw')
         ec_rw_version = self._ExtractFrid(self._BaseDirPath(IMAGE_EC),
-                                          'RW_FWID')
+                                          'RW_FRID')
         print('EC (RW) version: %s' % ec_rw_version, file=self._versions)
 
     if self._args.pd_image:
@@ -544,7 +544,7 @@ class FirmwarePacker(object):
         self._MergeRwEcFirmware(self._BaseDirPath(IMAGE_PD),
                                 self._BaseDirPath(IMAGE_MAIN), 'pdrw')
         pd_rw_version = self._ExtractFrid(self._BaseDirPath(IMAGE_PD),
-                                          'RW_FWID')
+                                          'RW_FRID')
         print('PD (RW) version: %s' % pd_rw_version, file=self._versions)
 
   def _CopyFile(self, src, dst, mode=CHMOD_ALL_READ):
